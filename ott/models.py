797e5f869e87aef92f7891ae10e73f3780301bab
aksh-56 <64796574+aksh-56@users.noreply.github.com>
2021-09-21 23:34:34 +0530

    updated models

--- ott/models.py
+++ ott/models.py
@@ -1,42 +1,42 @@
 from django.db import models
 
 class AdminMaster(models.Model):
-    admin_un= models.Autofield(primary=True,unique=True,maxlength=8)
-    admin_pass=models.Charfield(maxlength=10)
+    admin_un= models.AutoField(primary=True,unique=True,maxlength=8)
+    admin_pass=models.CharField(maxlength=10)
 
 class User(models.Model):
-    user_un = models.Autofield(primary=True, unique=True, maxlength=8)
-    user_name=models.Charfield(maxlength=25)
-    user_email=models.models.EmailField(max_length=254,unique=True)
-    user_pw = models.Charfield(maxlength=12)
-    user_ChannelName = models.Charfield(maxlength=25,unique=True)
+    user_un = models.AutoField(primary=True, unique=True, maxlength=8)
+    user_name=models.CharField(maxlength=25)
+    user_email=models.EmailField(max_length=254,unique=True)
+    user_pw = models.CharField(maxlength=12)
+    user_ChannelName = models.CharField(maxlength=25,unique=True)
 
 class Channel(models.Model):
-    Channel_Name = models.Charfield(maxlength=25, unique=True)
+    Channel_Name = models.CharField(maxlength=25, unique=True)
     Channel_wh = models.DurationField()
-    Channel_sub = models.Integerfield()
+    Channel_sub = models.IntegerField()
 
 class Media(models.Model):
-    m_id = models.Autofield(primary=True, unique=True, maxlength=8)
-    m_title = models.Charfield(maxlength=100)
+    m_id = models.AutoField(primary=True, unique=True, maxlength=8)
+    m_title = models.CharField(maxlength=100)
     m_desc = models.TextField(maxlength=350)
-    m_genre = models.Charfield(maxlength=20)
-    m_cat = models.Charfield(maxlength=10)
-    m_views = models.Integerfield()
-    m_likes = models.Integerfield()
-    m_actors = models.Charfield()
-    m_dir = models.Charfield()
+    m_genre = models.CharField(maxlength=20)
+    m_cat = models.CharField(maxlength=10)
+    m_views = models.IntegerField()
+    m_likes = models.IntegerField()
+    m_actors = models.CharField()
+    m_dir = models.CharField()
     m_date = models.DateField()
 
 class videos(models.Model):
-    v_id = models.Autofield(primary=True, unique=True, maxlength=8)
-    v_title = models.Charfield(maxlength=100)
+    v_id = models.AutoField(primary=True, unique=True, maxlength=8)
+    v_title = models.CharField(maxlength=100)
     v_desc = models.TextField(maxlength=350)
-    v_tags = models.Charfield(maxlength=200)
-    v_cat = models.Charfield(maxlength=10)
-    v_views = models.Integerfield()
-    v_likes = models.Integerfield()
-    v_wh = models.Integerfield()
+    v_tags = models.CharField(maxlength=200)
+    v_cat = models.CharField(maxlength=10)
+    v_views = models.IntegerField()
+    v_likes = models.IntegerField()
+    v_wh = models.IntegerField()
 
 #akshhhhhhh!
 

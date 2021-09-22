7c6dedb99fe55c41a7b5891f28921bf42d4c8dab
Vinay Kabadagi <vinaykabadagi@gmail.com>
2021-09-22 11:23:01 +0530

    space

--- ott/models.py
+++ ott/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
+
 class AdminMaster(models.Model):
-    admin_un= models.CharField(primary_key=True,unique=True,max_length=8)
-    admin_pass=models.CharField(max_length=10)
+    admin_un = models.CharField(primary_key=True, unique=True, max_length=8)
+    admin_pass = models.CharField(max_length=10)
+
 
 class User(models.Model):
     user_un = models.AutoField(primary_key=True, unique=True, max_length=8)
-    user_name=models.CharField(max_length=25)
-    user_email=models.EmailField(max_length=254,unique=True)
+    user_name = models.CharField(max_length=25)
+    user_email = models.EmailField(max_length=254, unique=True)
     user_pw = models.CharField(max_length=12)
-    user_ChannelName = models.CharField(max_length=25,unique=True)
+    user_ChannelName = models.CharField(max_length=25, unique=True)
+
 
 class Channel(models.Model):
     Channel_Name = models.CharField(max_length=25, unique=True)
     Channel_wh = models.DurationField()
     Channel_sub = models.IntegerField()
+
 
 class Media(models.Model):
     m_id = models.AutoField(primary_key=True, unique=True, max_length=8)
@@ -25,8 +29,9 @@ class Media(models.Model):
     m_views = models.IntegerField()
     m_likes = models.IntegerField()
     m_actors = models.CharField(max_length=200)
-    m_dir = models.CharField(max_length=200)
+    m_dir = models.CharField(max_length=200)x
     m_date = models.DateField()
+
 
 class videos(models.Model):
     v_id = models.AutoField(primary_key=True, unique=True, max_length=8)
@@ -38,7 +43,6 @@ class videos(models.Model):
     v_likes = models.IntegerField()
     v_wh = models.IntegerField()
 
-#akshhhhhhh!
+# akshhhhhhh!
 
 # Create your models here.
-

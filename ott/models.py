c7588f4607f981ed53950362ff02d1e459cd8e52
Rohan <rohanpatil9529@gmail.com>
2021-09-22 11:51:35 +0530

    models.py update

--- ott/models.py
+++ ott/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
 class AdminMaster(models.Model):
-    admin_un= models.CharField(primary_key=True,unique=True,max_length=8)
+    admin_id=models.AutoField(primary_key=True,unique=True)
+    admin_un= models.CharField(unique=True,max_length=8)
     admin_pass=models.CharField(max_length=10)
+    admin_status = models.CharField(max_length=8)
 
 class User(models.Model):
-    user_un = models.AutoField(primary_key=True, unique=True, max_length=8)
+    user_id = models.AutoField(primary_key=True,unique=True)
+    user_un = models.CharField( unique=True,max_length=20)
     user_name=models.CharField(max_length=25)
     user_email=models.EmailField(max_length=254,unique=True)
     user_pw = models.CharField(max_length=12)
     user_ChannelName = models.CharField(max_length=25,unique=True)
+    user_status = models.CharField(max_length=8)
 
 class Channel(models.Model):
-    Channel_Name = models.CharField(max_length=25, unique=True)
-    Channel_wh = models.DurationField()
-    Channel_sub = models.IntegerField()
+    channel_Name = models.CharField(max_length=25, unique=True)
+    channel_wh = models.DurationField()
+    channel_sub = models.IntegerField()
+    channel_status = models.CharField(max_length=8)
 
 class Media(models.Model):
-    m_id = models.AutoField(primary_key=True, unique=True, max_length=8)
+    m_id = models.AutoField(primary_key=True, unique=True)
     m_title = models.CharField(max_length=100)
     m_desc = models.TextField(max_length=350)
     m_genre = models.CharField(max_length=20)
@@ -27,9 +32,13 @@ class Media(models.Model):
     m_actors = models.CharField(max_length=200)
     m_dir = models.CharField(max_length=200)
     m_date = models.DateField()
+    m_status = models.CharField(max_length=8)
+    m_movies = models.ImageField(upload_to="ott/static/media/images_movies")
+    m_series = models.ImageField(upload_to="ott/static/media/images_series")
+
 
 class videos(models.Model):
-    v_id = models.AutoField(primary_key=True, unique=True, max_length=8)
+    v_id = models.AutoField(primary_key=True, unique=True)
     v_title = models.CharField(max_length=100)
     v_desc = models.TextField(max_length=350)
     v_tags = models.CharField(max_length=200)
@@ -37,8 +46,9 @@ class videos(models.Model):
     v_views = models.IntegerField()
     v_likes = models.IntegerField()
     v_wh = models.IntegerField()
-
-#akshhhhhhh!
+    v_image = models.ImageField(upload_to="ott/static/media/images_videos")
+    v_status = models.CharField(max_length=8)
+    
 
 # Create your models here.
 

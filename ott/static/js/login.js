cff997f6f1f88683032e214b27685e3635664257
Vinay Kabadagi <vinaykabadagi@gmail.com>
2021-09-24 00:35:54 +0530

    u

--- ott/static/js/login.js
+++ ott/static/js/login.js
@@ -1,35 +1,35 @@
-$(document).ready(function() {
-	$("#login").click(function(){
-		if($("#name").val() == "") {
-			alert("Please Enter Name");
-		}
-		if($("#password").val() == "") {
-			alert("Please Enter Password");
-		}
+$(document).ready(function () {
+  $("#login").click(function () {
+    if ($("#name").val() == "") {
+      alert("Please Enter Name");
+    }
+    if ($("#password").val() == "") {
+      alert("Please Enter Password");
+    }
 
-let formData = new FormData();
+    let formData = new FormData();
     formData.append("name", $("#name").val());
-	formData.append("password", $("#password").val());
-	formData.append(
-		"csrfmiddlewaretoken",
-		$("input[name=csrfmiddlewaretoken]").val());
+    formData.append("password", $("#password").val());
+    formData.append(
+      "csrfmiddlewaretoken",
+      $("input[name=csrfmiddlewaretoken]").val()
+    );
 
-$.ajax({
-	url: "/user_login/",
-	type: "POST",
-	data: formData,
-	processData: false,
-	contentType: false,
-	success: function (response) {
-		if (response == "11") {
-		alert("Login successful");
-		window.location = "/home/";
-	}
-	else if(response == "12"){
-		alert("Login not successful,Register!");
-		window.location = "/register/";
-	}
-}
-});
-});
+    $.ajax({
+      url: "/user_login/",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function (response) {
+        if (response == "11") {
+          alert("Login successful");
+          window.location = "/home/";
+        } else if (response == "12") {
+          alert("Login not successful,Register!");
+          window.location = "/register/";
+        }
+      },
+    });
+  });
 });
